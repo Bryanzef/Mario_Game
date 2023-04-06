982d559683f852ea8cdb9c97882ed2dbff75dd95
Bryanzef <bryanzef7@gmail.com>
2023-04-06 19:23:40 -0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,55 @@
+//Pega o evento de clique
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+
+const jump = () => {
+  mario.classList.add("jump");
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
+};
+
+const loop = setInterval(() => {
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(mario)
+    .bottom.replace("px", "");
+
+  //Quando encostar no tubo a animação para !
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 88) {
+    /* */
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = "none";
+    mario.style.bottom = `${marioPosition}px`;
+
+    mario.src = "/assets/game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "48px";
+
+    clearInterval();
+  }
+}, 10);
+
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  // Dispositivo móvel - adiciona o evento de toque na tela
+  document.addEventListener("touchstart", jump);
+} else {
+  // Computador - adiciona o evento de keyup e remove o evento de keydown
+  const keyDownHandler = () => {
+    document.removeEventListener("keydown", keyDownHandler);
+  };
+
+  document.addEventListener("keydown", keyDownHandler);
+
+  document.addEventListener("keyup", function (event) {
+    if (event.code === "Space") {
+      jump();
+    }
+  });
+}
